0629880474322437f4b53132033ba181a12e1c65
AnastasiiaDm <anastasia8sergev@gmail.com>
2019-01-21 17:07:05 +0200

    timestamp added

--- cypress/integration/GET_JSON.js
+++ cypress/integration/GET_JSON.js
@@ -1,23 +1,9 @@
-// describe("My first test", function(){
-//     it ("Visits the kitchen sink", function(){
-//     cy. visit('https://jasper-staging.herokuapp.com/api-docs/index.html')
-    
-//     cy.contains('login').click()
-    
-//     cy.contains('Try it out').click()
-    
-//     cy.contains('Execute').click()
-    
-    
-    
-//     })
-    
-//     })
-
 var request = require('request');
 
 var path = require('path'),
     fs = require('fs');
+
+    var dateFormat = require('dateformat');
 
 var authData = {
   "auth": {
@@ -28,9 +14,18 @@ var authData = {
 
 var jwt;
 
-function getUnitsByID(id){
+function timeStamp() {
+
+       
+    var day=dateFormat(new Date(), "yyyy-mm-dd h..MM..ss");  
+    console.log(day)       
+      return day;
+          
+      }
+
+function getUnitsByID(id, timeInMs){
     request({
-        url: "https://jasper-staging.herokuapp.com/api/v1/units1111/"+id,
+        url: "https://jasper-staging.herokuapp.com/api/v1/units/"+id,
         method: "GET",
         json: true,   // <--Very important!!!
         headers: {
@@ -39,7 +34,8 @@ function getUnitsByID(id){
     }, function (error, response, body){
         if (!error && response.statusCode == 201||200) {
             console.log("unit data:", "\n", body)
-            ensureDirectoryExistence('C:/Users/user/Downloads/JSON/test.txt', JSON.stringify(body));
+            ensureDirectoryExistence('C:/Users/user/Downloads/JSON('+timeInMs+')/test.txt', JSON.stringify(body));
+
         }
         else{
             console.log("unit data error", error);
@@ -51,12 +47,13 @@ function getUnitsByID(id){
 function ensureDirectoryExistence(filePath, body) {
 
     var dirname = path.dirname(filePath);
-    console.log(dirname);
     if (fs.existsSync(dirname)) {
-        console.log('dirname exist');
+        console.log(dirname)
       return true;
     }
-    ensureDirectoryExistence(dirname);
+    ensureDirectoryExistence(dirname,);
+    console.log(dirname)
+
     fs.mkdirSync(dirname);
 
     fs.writeFileSync(filePath, body, function(err) {
@@ -83,7 +80,7 @@ function getJson(){
             jwt = body.jwt;
             console.log("token:", jwt);
 
-            getUnitsByID(1);
+            getUnitsByID(1, timeStamp());
 
         }
         else{
